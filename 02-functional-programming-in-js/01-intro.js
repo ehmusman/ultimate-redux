8f6c96e2f7ae21416ad9514856147e5388bdaa8a
ehmusman <ehmusman@gmail.com>
2021-03-16 12:38:39 +0500

    pure functions are covered

--- 02-functional-programming-in-js/01-intro.js
+++ 02-functional-programming-in-js/01-intro.js
@@ -37,21 +37,113 @@
 
 
 // function can also return a function
-function sayHello() {
-    return function () { // in this case this is an anonymous function
-        return "Hello World"
+// function sayHello() {
+//     return function () { // in this case this is an anonymous function
+//         return "Hello World"
+//     }
+// }
+// let fn = sayHello()
+// let message = fn();
+
+// // Higher order function
+// // higher order function is a funcition which take a function as an argument or it return a function. or both combinely
+// // it's possible we have used higher order function without knowing about it. like
+// const numbers = [1, 2, 3, 4]
+// numbers.map(number => number * 2) // this is an example of higher order function
+// // an other example is 
+// setTimeout(() => {
+//     console.log("Hello world")
+// }, 1000);
+// // this is also called higher order function because it take a function as an argument
+
+/////////////////////////////////////////////////////////////
+
+// Function composition
+// in functional programming we make different small functions and than compose them to solve a real world complex problem
+
+// here we have a problem
+// let input = "  Javascript  "
+// let output = "<div>" + input.trim() + "</div>"
+
+// we have to trim the input first than we have to wrap it in the div element
+// lets solve this by using functional programming
+// let trim = str => str.trim()
+// let wrap = str => `<div>${str}</div>`
+// let toLowerCase = str => str.toLowerCase()
+
+// let input = "  Javascript  "
+// const output = wrap(toLowerCase(trim(input)))
+
+// here is the only problem of paranthesis. as the problem becomes more and more comples the paranthesis become increases. that is a pain in the neck. we'll see how to solve this problem
+
+/////////////////////////////////////////////
+
+// Composing and piping
+// install lodash and use compose and pipe from it
+import { compose, pipe } from 'lodash/fp'
+// let trim = str => str.trim()
+// let wrap = str => `<div>${str}</div>`
+// let toLowerCase = str => str.toLowerCase()
+
+// let input = "  Javascript  "
+
+// // const transform = compose(wrap, toLowerCase, trim) // here order matters alot from right to left. for order from left to right we can use pipe instead of compose
+// const transform = pipe(trim, toLowerCase, wrap)
+// transform(input)
+
+
+//////////////////////////////////////////////////////////////////////
+// Currying a function
+// at some instant of time if we need to change the div with span. that how this will happen. we'll go manually to change the div.
+// we'll create one function with two input arguments
+
+let trim = str => str.trim()
+let wrap = type => str => `<${type}>${str}</${type}>`
+let toLowerCase = str => str.toLowerCase()
+
+let input = "  Javascript  "
+
+// const transform = compose(wrap, toLowerCase, trim) // here order matters alot from right to left. for order from left to right we can use pipe instead of compose
+const transform = pipe(trim, toLowerCase, wrap("div")) // we have added to arguments. but in pipe function we only pass one argument in the function
+
+console.log(transform(input))
+
+// how to solve this dual argument problem? 
+function add1(a) {
+    return function (b) {
+        return a + b
     }
 }
-let fn = sayHello()
-let message = fn();
+add1(1)(5) // add(1) will return a function we'll call that function with (5) these paranthesis with passing 5. (a,b) => a+b
 
-// Higher order function
-// higher order function is a funcition which take a function as an argument or it return a function. or both combinely
-// it's possible we have used higher order function without knowing about it. like
-const numbers = [1, 2, 3, 4]
-numbers.map(number => number * 2) // this is an example of higher order function
-// an other example is 
-setTimeout(() => {
-    console.log("Hello world")
-}, 1000);
-// this is also called higher order function because it take a function as an argument
+// in pipe() // output of first is the input of second, output of second becomes input of third and so on
+
+
+///////////////////////////////////////////////////////////
+
+// Pure function
+// a function is pure function which return same result when same arguiment is passed to it.
+function add(a, b) {
+    return a + b
+}
+add(1, 2)
+// this is the example of pure function
+function random(a) {
+    return Math.random() * a
+}
+random(5)
+ // this is not a pure function
+
+ // so in Pure function
+ // 1- we dont get random values
+ // no currnt date/time
+ // we canot read or change global state (DOM, files, db etc)
+// in practice not every function is a pure.
+// in practice we have special functions called reducers. we have to make it sure that the reducers should be pure and other function can be impure.
+// in pure functions we cannot mutate the parameters.
+
+// benefits of pure functions
+// self documenting
+// easily testable,
+// concurrency // means they cann be run parallel.
+// Cacheable
