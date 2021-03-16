e524cd651052f4bbdea534d63695962426a2d5f9
ehmusman <ehmusman@gmail.com>
2021-03-16 11:28:20 +0500

    course structure covered

---
+++ 01-Getting-started/04-course-structure.js
@@ -0,0 +1,13 @@
+// we'll learn
+
+// 1- Functional programming principles.
+// 2- Fundamentals of redux(reducers, actions, action-creaters and so on)
+// 3- Build redux from scratch
+// 4- Debugging of redux application
+// 5- writing clean,concise, maintable and beautiful redux code.
+// 6- Designing a redux store(some techniques to build real world complex applications)
+// 7- middleware
+// 8- calling APIs
+// 9- Testing redux App
+// 10- Integration with react
+
