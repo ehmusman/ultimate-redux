90fb45161b69c38ed3052446f7a439d734b5b856
ehmusman <ehmusman@gmail.com>
2021-03-16 10:45:49 +0500

    when to use redux

---
+++ 01-Getting-started/02-useRedux?.js
@@ -0,0 +1,10 @@
+// When not to use Reduxx
+
+// Tight Budget // because it may add extra space to the server
+// small and medium sized app
+// Simple UI data flow
+// Static Data
+
+// if we are hammer than every thing will looking like a hammer
+// so think deeply if we have to use the reduxx or not. it may slow down the performance of application when we use it where its not need.
+// be a creative problem solver
