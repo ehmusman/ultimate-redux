532f1b8f5319f2355499005229b8bef1432efaad
ehmusman <ehmusman@gmail.com>
2021-03-16 10:45:37 +0500

    pros and cons of redux

---
+++ 01-Getting-started/01-Pros-Cons-redux.js
@@ -0,0 +1,13 @@
+// Pros
+// 1- Predictable State Change
+// 2- Centralized State
+// 3- Easy Debugging
+// 4- easily Cache or Preserve page State
+// 5- easily implementation of Undo and Redo features
+// the Best thing is that it can work with any type of UI library like React, angular, Vue and even with vanilla javascript
+// 6- large and growing ecosystems of add-ons
+
+
+//  CONS 
+// 1- redux introduced some complexity in code. because redux is based on functional programming principle.
+// Verbosity (means we have to setup the boilerplate code to get things done)
