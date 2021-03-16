da424d6dd68ac3cad96581c767e26352b569fb34
ehmusman <ehmusman@gmail.com>
2021-03-16 16:43:23 +0500

    dispatched an action to add and remove a bug

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+import store from './store'
+
+console.log(store)
+
+// store.dispatch({
+//     type: "bugAdded",
+//     payload: {
+//         id: Math.random() * 123456765432,
+//         description: "Bug1",
+//         resolved: false
+//     }
+// })
+store.dispatch({
+    type: "bugRemoved",
+    payload: {
+        id: 2086970775.3295555
+    }
+})
+
+console.log(store.getState())
--- redux-starter/src/reducer.js
+++ redux-starter/src/reducer.js
@@ -7,7 +7,7 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
-        case "begAdded":
+        case "bugAdded":
             return {
                 ...state,
                 bugs: [action.payload, ...state.bugs]
