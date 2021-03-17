cef9d0cea192b721fac1a5bdb3e7acf8493c3c68
ehmusman <ehmusman@gmail.com>
2021-03-17 16:31:56 +0500

    reducer is created

--- 06-writing-clean-redux-code/redux-toolkit/intro.js
+++ 06-writing-clean-redux-code/redux-toolkit/intro.js
@@ -33,8 +33,30 @@
 // console.log(addBug({id: 1}))
 // { type: "addBug"
 //      payload: {
-    // id: 1
+// id: 1
 // }
 // }
 
 // now we dont need to ccreate constants and saperate action creators. pass the payload where we call these functions
+
+////////////////////////////////////////////////////////////////
+// createReducer
+
+// its basically used to get rid of switch case statements
+// in this case we are not writing the switch case statements. we'll write regular mutating code. just like we write code outside the redux. under the hood redux tool kit use immer so the mutating code that we write automatically get translated to immutable update pattern
+
+// import {createReducer} from  "@reduxjs-toolkit"
+
+createReducer(initialState, {
+    // key: value
+    // actions: functions to handle the actions like
+    // event: eventHandler
+    // all the actions we have created using the actionCreator
+    // like bugAdded, bugUpdated, bugRemoved
+    // the function agains each action has two parameters, state and actions such like the normal reducer in redux
+
+})
+// createReducer is a method that takes two arguments.
+// 1- initial state, 2- object
+// second parameter is an object that maps actions to functions that handles those actions.
+//
